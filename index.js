5f670f8c8d6b9c0c2cf958146d5611ad4fb36501
Lovro Boric <lovro.boric@gmail.com>
2022-05-06 03:19:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const sdsa = " edasd";
+const number = 2;
+const array = [2, 4, 54, 5, 5, 5, 5];
+
+console.log(array);
+
+array.filter((el) => el > 3);
+
+console.log(number);
+
+const square = number * number;
